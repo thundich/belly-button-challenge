8725598f0c244c3ee8b881d63a32a3c3dcef67b2
thundich <tamaradurgan@gmail.com>
2024-01-02 23:48:17 -0500

    Final commit to GitHub

--- static/js/app.js
+++ static/js/app.js
@@ -7,35 +7,48 @@ var sample_data = {}
 function init() {
     d3.json(url).then((data) => {
       // Save the data for use later
-      sample_data = data;
+      sample_data = data
       // Create the drop down from the names in the data
-      let dropDown = d3.select("#selDataset");
+      let dropDown = d3.select("#selDataset")
       data.names.forEach((id) => {
         dropDown.append("option").text(id).property("value", id)
       })
 
       barChart(data.names[0], data)
       bubbleChart(data.names[0], data)
+      metaData(data.names[0], data)
     }
   )
 }
 
-function barChart(sample_name, data) {
+function getSampleInfo(sample_name, data) {
     // Retrieve all sample data
-    let samples = data.samples;
+    let samples = data. samples 
 
-    // Find the sample with the correct sample_name
-    let sample = samples.find(item => item.id == sample_name);
+    // Find the sample with the correct sample sample_name
+    let sample = samples.find(item => item.id == sample_name)
 
-    // Get the otu_ids, otu_lables, and sample_values
-    let otu_ids = sample.otu_ids;
-    let otu_labels = sample.otu_labels;
-    let sample_values = sample.sample_values;
+    // Return the otu_ids, sample_values, otu_labels
+    return [sample.otu_ids, sample.sample_values, sample.otu_labels]
+}
+
+function metaData(sample_name, data){ 
+    let metadata = data.metadata.find(item => item.id == sample_name)
+    d3.select("#sample-metadata").html("")
+
+    // Use Object.entries to add each key/value pair to the panel
+    Object.entries(metadata).forEach(([key,value]) => {
+        d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`)
+    })
+}
+
+function barChart(sample_name, data) {
+    let sample_info = getSampleInfo(sample_name, data)
 
     // Set top ten items to display in descending order
-    let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
-    let xticks = sample_values.slice(0,10).reverse();
-    let labels = otu_labels.slice(0,10).reverse();
+    let yticks = sample_info[0].slice(0,10).map(id => `OTU ${id}`).reverse()
+    let xticks = sample_info[1].slice(0,10).reverse()
+    let labels = sample_info[2].slice(0,10).reverse()
 
     // Set up the trace for the bar chart
     let trace = {
@@ -44,12 +57,12 @@ function barChart(sample_name, data) {
         text: labels,
         type: "bar",
         orientation: "h"
-        };
+        }
 
     // Setup the layout
     let layout = {
         title: "Top 10 OTUs Present"
-        };
+        }
 
     // Call Plotly to plot the bar chart
     Plotly.newPlot("bar", [trace], layout)
@@ -58,45 +71,37 @@ function barChart(sample_name, data) {
 
 function optionChanged(value) { 
     // Call all functions 
-    barChart(value, sample_data);
+    barChart(value, sample_data)
     bubbleChart(value, sample_data)
+    metaData(value, sample_data)
 }
 
 function bubbleChart(sample_name, data) { 
-    // Retrieve all sample data
-    let samples = data.samples;
-
-    // Find the sample with the correct sample_name
-    let sample = samples.find(item => item.id == sample_name);
-
-    // Get the otu_ids, otu_lables, and sample_values
-    let otu_ids = sample.otu_ids;
-    let otu_labels = sample.otu_labels;
-    let sample_values = sample.sample_values;
-
+    let sample_info = getSampleInfo(sample_name, data)
+   
     // Set up the trace for the bubble chart
     let trace = {
-        x: otu_ids,
-        y: sample_values,
-        text: otu_labels,
+        x: sample_info[0],
+        y: sample_info[1],
+        text: sample_info[2],
         mode: "markers", 
         marker: {
-            size: sample_values,
-            color: otu_ids, 
+            size: sample_info[1],
+            color: sample_info[0], 
             colorscale: 'YlGnBu'
         }
     
-        };
+        }
     // Set up the layout
     let layout = {
         title: "Bacteria Per Sample",
         hovermode: "closest",
         xaxis: {title: "OTU ID"},
-        };
+        }
 
     // Call Plotly to plot the bubble chart
     Plotly.newPlot("bubble", [trace], layout)
 }
 
 // Call the initialize function
-init();
+init()
